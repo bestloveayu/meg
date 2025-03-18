bc164b566682bcbffc8f435dc3889de9fdddc8de
bestloveayu <bestloveayu@gmail.com>
2025-03-19 00:38:37 +0800

    script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function verifyPasscode() {
 
     if (passcode === correctPasscode) {
         // 驗證成功，跳轉到指定網頁
-        window.location.href = "https://ooopenlab.cc/preview/xBbpWeqHqkpuN5n4SbgZ"; // 替換為你的目標網頁
+        window.open = "https://ooopenlab.cc/preview/xBbpWeqHqkpuN5n4SbgZ", "_blank"; // 替換為你的目標網頁
     } else {
         // 驗證失敗，顯示新的警告文字
         message.textContent = "不要亂猜!再好好整理重點吧!";
