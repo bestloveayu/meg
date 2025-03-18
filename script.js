4e1120c05169237d302451c815fcfc41f47362c7
bestloveayu <bestloveayu@gmail.com>
2025-03-19 00:46:56 +0800

    script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function verifyPasscode() {
     const passcode = document.getElementById("passcode").value;
-    const correctPasscode = "成本服務美味"; // 修改為新的六字密碼
+    const correctPasscode = "成本服務美味"; // 通關密碼
     const message = document.getElementById("message");
 
     if (passcode === correctPasscode) {
-        // 驗證成功，跳轉到指定網頁
-        window.open = "https://ooopenlab.cc/preview/xBbpWeqHqkpuN5n4SbgZ", "_blank"; // 替換為你的目標網頁
+        // 驗證成功，在新分頁中開啟指定網頁
+        window.open("https://ooopenlab.cc/preview/xBbpWeqHqkpuN5n4SbgZ", "_blank"); // 替換為你的目標網頁
     } else {
-        // 驗證失敗，顯示新的警告文字
+        // 驗證失敗，顯示警告
         message.textContent = "不要亂猜!再好好整理重點吧!";
     }
 }
